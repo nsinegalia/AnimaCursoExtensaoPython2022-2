1d0b873d82faba7d219faa809e338acc2952f16c
Nicolas Sinegalia <61257114+nsinegalia@users.noreply.github.com>
2022-11-02 20:48:42 +0000

    Aula1. Aprendemos a exibir 3 maneiras diferentes com variaveis

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # Meu primeiro projeto Python!!!
 #
 # print() = comando de saida
+'''
+Comentário em bloco
+
+'''
 print('Alo mundo!')
 
 # Quando quiser guardar uma String!
@@ -9,11 +13,11 @@ nome = "Nicolas Sinegalia"
 idade = 23
 
 #Exibir o meu nome que está na variável
-print("O seu nome é " + nome)
-print("A sua idade é " + idade)
+#Quando quiser exibir frase + variável
+print("O seu nome é " + nome) #string
+print("A sua idade é "+str(idade)) #int
+print(f"A sua idade é {idade} anos \n") #int
+print("Minha idade é {} anos".format(idade))
 
-
-'''
-Comentário em bloco
-
-'''
+#Quando quiser exibir nome + idade
+print ("Meu nome é {} e tenho {} anos".format(nome,idade))
