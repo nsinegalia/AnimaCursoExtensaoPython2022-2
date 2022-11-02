8dea6686da61de5dc3451277056e27460c2e2928
Nicolas Sinegalia <61257114+nsinegalia@users.noreply.github.com>
2022-11-02 20:29:48 +0000

    Atualizações

--- main.py
+++ main.py
@@ -3,6 +3,14 @@
 # print() = comando de saida
 print('Alo mundo!')
 
+# Quando quiser guardar uma String!
+nome = "Nicolas Sinegalia"
+# Quando quiser guardar um número inteiro
+idade = 23
+
+#Exibir o meu nome que está na variável
+print("O seu nome é " + nome)
+print("A sua idade é " + idade)
 
 
 '''
