410097a0b01d4bde2c6d1a74bed873d60183480b
Nicolas Sinegalia <61257114+nsinegalia@users.noreply.github.com>
2022-11-06 23:01:56 +0000

    Aula completa 2022-10-26

--- main.py
+++ main.py
@@ -1,34 +1,20 @@
-# comando input(): quero permitir que
-# o usuário digite algo...
-nome = input("Digite seu nome: ")
-#pede a idade para o usuário "Qual sua idade?"
-idade = int(input("Digite sua idade: "))
+#Exemplo de laço
 
-#comando de saída..exibir na tela
-print(f"Boa noite, seu nome é {nome}")
-#exiba "Sua idade é ..."
-print("Sua idade é {}".format(idade))
+#Se eu quisesse exibir números de 1 a 10?
+print("Aqui mostramos de maneira 'rústica'")
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+print("6")
+print("7")
+print("8")
+print("9")
+print("10")
 
-#e se eu quisesse mostrar o DOBRO da idade informada?
-dobro = idade * 2
-print("O dobro da idade informada é {}".format(dobro))
-
-#Estrutura condicional - o famoso "SE" (if)
-#Se a pessoa for maior de idade, mostre "Você é maior de idade, ótimo! Já pode beber ou dirigir"
-if idade >= 18:
-  print("Você é maior de idade, ótimo! Já pode beber ou dirigir")
-else:
-  print("Você é xóven ainda, xóven ainda...")
-
-#E se eu quisessem perguntar o gênero (M = Masculino e F = Feminino)
-#Mostre (...e você também precisa/precisou prestar o serviço militar obrigatório)
-genero = input("Informe o gênero M=Masculino, F=Feminino, O=Outros: ")
-if idade >= 18 and genero == "M":
-  print("... e você também precisa/precisou prestar o serviço militar obrigatório")
-
-
-
-
-
-
-print("vai ser executada de qualquer jeito")
+print("Aqui é do jeito certo...")
+numero = 1
+print(numero)
+numero = numero + 1
+print(numero)
