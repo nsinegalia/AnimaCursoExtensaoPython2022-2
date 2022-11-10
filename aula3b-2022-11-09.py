7e262b22091a992a01e359d03c4685ccc25b43ba
Nicolas Sinegalia <61257114+nsinegalia@users.noreply.github.com>
2022-11-10 01:01:55 +0000

    22h

--- aula3b-2022-11-09.py
+++ aula3b-2022-11-09.py
@@ -3,21 +3,28 @@
 preco = 1999.90
 
 # Calcular 5% de imposto, guardar na variavel imposto e exibir na tela
-imposto = preco * 0.05
+imposto = preco * 0.07
 print(imposto)
 
 preco2 = 100
-imposto2 = preco2 * 0.05
+imposto2 = preco2 * 0.07
 print(imposto2)
 
 #Criar uma função calcular_imposto() que calcular um imposto de 5% e retorna quem pediu
 
 def calcular_imposto(preco_produto):
-  imposto = preco_produto * 0.05
+  imposto = preco_produto * 0.07
   return imposto
 
 
 #Aqui é o uso.. aqui é imposto a calcular..
 preco = 299
 imposto = calcular_imposto(preco)
-print(imposto)
+print(f"Esse aqui é com a função (7%): {imposto}")
+
+#agora calcular imposto a alíquota agora é 7%
+
+valores = [1.99,24.50,78.27,1515.5]
+# se eu quiser calcular o imposto destes quatro valores... e exibir na tela assim: "O imposto de.... é ..." (1o. preço, 2o. imposto)
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto(valor)}")
